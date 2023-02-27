01ad45e21bec0ea4041bb5bd61adc57d4135e692
Staninna <26458805+Staninna@users.noreply.github.com>
2023-02-27 09:41:57 +0100

    Move cam with player

--- src/entity/camera.rs
+++ src/entity/camera.rs
@@ -1,12 +1,25 @@
 use bevy::prelude::*;
 
+use crate::entity::player::Player;
+
 pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_camera);
+        app.add_startup_system(spawn_camera).add_system(move_camera);
     }
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+fn move_camera(
+    mut camera: Query<&mut Transform, With<Camera>>,
+    player: Query<&Transform, (With<Player>, Without<Camera>)>,
+) {
+    for mut camera_transform in camera.iter_mut() {
+        for player_transform in player.iter() {
+            camera_transform.translation = player_transform.translation;
+        }
+    }
 }
