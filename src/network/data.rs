3c1f6bac0543ba430b91965d5e2c0a3d43a940c7
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-06 01:45:50 +0100

    Created a better struct for input over network using bitflags

--- src/network/data.rs
+++ src/network/data.rs
@@ -3,5 +3,16 @@ use bytemuck::{Pod, Zeroable};
 #[repr(C)]
 #[derive(Copy, Clone, PartialEq, Eq, Pod, Zeroable)]
 pub struct UserInput {
-    pub input: u8,
+    pub input: KeyInput,
+}
+
+bitflags::bitflags! {
+    #[repr(C)]
+    #[derive(Pod, Zeroable)]
+    pub struct KeyInput: u8 {
+        const UP    = 0b0000_0001;
+        const LEFT  = 0b0000_0010;
+        const DOWN  = 0b0000_0100;
+        const RIGHT = 0b0000_1000;
+    }
 }
