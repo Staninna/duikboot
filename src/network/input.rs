cc21f5057ab3f003250acf63c51c264bf0a21037
Staninna <26458805+Staninna@users.noreply.github.com>
2023-03-08 11:32:03 +0100

    Removed useless comment

--- src/network/input.rs
+++ src/network/input.rs
@@ -3,7 +3,6 @@ use crate::settings::player::{KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_UP};
 use bevy::prelude::*;
 use ggrs::PlayerHandle;
 
-// Needs to have the same logic as all systems that use input (keyboard_input: Res<Input<KeyCode>>)
 pub fn input(_handle: In<PlayerHandle>, keyboard_input: Res<Input<KeyCode>>) -> UserInput {
     // Get user input
     let mut input = KeyInput::empty();
